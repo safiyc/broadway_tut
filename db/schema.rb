36c4da907e02a00a1e99da5e1232ec6db4e1ab2e
safiy cham <csafi13@yahoo.com>
2017-11-29 17:32:03 -0800

    add Category Model and assinged Plays to Users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130005558) do
+ActiveRecord::Schema.define(version: 20171130012846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 20171130005558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
