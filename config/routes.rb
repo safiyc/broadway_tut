465ecde1cb1b262c14e5766ca58ec3e90f9c6e30
safiy cham <csafi13@yahoo.com>
2017-11-30 11:30:01 -0800

    add routes for reviews and review controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :plays
+  resources :plays do
+    # 'reviews' nested inside 'plays' bc 'reviews' routes come after 'plays'
+    resources :reviews
+  end
   root 'plays#index'
 
 end
